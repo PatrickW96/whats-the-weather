77559d49100a3a2152edc00e02f7f413084b113e
Patrick W <pat.realrealestatellc@gmail.com>
2020-09-24 18:18:31 -0400

    Adding more files. I have an operational current city card icon and almost all else. Still need to bring UVI number to card. Test one

--- scripts/script.js
+++ scripts/script.js
@@ -1,11 +1,12 @@
 $(document).ready(function() { 
     $('#submitBtn').on('click' , function(event) {
         event.preventDefault();
+        $('#currentCity').remove();
 
         var searched = $('#citySearched').val();
 
         var APIkey = '889fa96c2ff3642885f0a0352803b7d4';
-        var curentWeatherQueryURL = `http://api.openweathermap.org/data/2.5/weather?q=${searched}&appid=${APIkey}`;
+        var curentWeatherQueryURL = `http://api.openweathermap.org/data/2.5/weather?q=${searched}&appid=${APIkey}&units=imperial`;
     
         $.ajax({
             url: curentWeatherQueryURL,
@@ -13,10 +14,18 @@ $(document).ready(function() {
         }).then(function(response) {
             var result = response;
             var date = new Date(result.dt * 1000).toLocaleDateString();
+            var currentCity = `<div id="currentCity" class="border my-3 py-3">` +
+            `<h3 class="ml-4 p-1 pb-2">${result.name} (${date}) <img src="http://openweathermap.org/img/wn/${result.weather[0].icon}@2x.png"></h3>` +
+            `<p class="ml-4 p-1">${result.main.temp} &deg;F</p>` +
+            `<p class="ml-4 p-1">${result.main.humidity} %</p>` +
+            `<p class="ml-4 p-1">${result.wind.speed} MPH</p>` +
+            `<p class="ml-4 p-1">UV index: 9.46</p>` +
+           ` </div>`;
 
+           $('#weatherContainer').append(currentCity);
             console.log(date);
             console.log(searched);
-            console.log(result.weather[0].icon);
+            console.log(result);
         });    
     });
 });
