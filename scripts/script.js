e5f0eb3da5b1df64cd15b65a2afb8d0dbf5b5e55
Patrick W <pat.realrealestatellc@gmail.com>
2020-09-28 13:25:14 -0400

    Updating js files. The searched histroy is displayed. Adding click event to set history after push.

--- scripts/script.js
+++ scripts/script.js
@@ -1,11 +1,16 @@
 $(document).ready(function () {
-    
+    var cityHistoryArray = [];
+
     $('#submitBtn').on('click', function (event) {
         event.preventDefault();
-        // $('#weatherContainer').clear();  //Removing the last city before appending the next.
 
+        var searched = $('#citySearched').val().trim();
+        cityHistoryArray.push(searched);
 
-        var searched = $('#citySearched').val();
+        displayHistory(); //Function used to display users searched history.
+        $('#weatherContainer').remove();  //Removing the last city before appending the next.
+
+        // var searched = $('#citySearched').val();
         var APIkey = '889fa96c2ff3642885f0a0352803b7d4';
         var curentWeatherQueryURL = `http://api.openweathermap.org/data/2.5/weather?q=${searched}&appid=${APIkey}&units=imperial`;
 
@@ -14,7 +19,7 @@ $(document).ready(function () {
             method: "GET"
         }).then(function (response) {
             displayMainCard(response);
-// ======================================================================================================================================
+
             var cityID = response.id;
             var futureForecastDiv = $(`<div>`).attr({ class: 'row mx-auto', id: 'futureForecastDiv' });
             var forecastURL = `http://api.openweathermap.org/data/2.5/forecast?id=${cityID}&appid=${APIkey}&units=imperial`;
@@ -23,23 +28,28 @@ $(document).ready(function () {
                 url: forecastURL,
                 method: "GET"
             }).then(function (response2) {
+
                 for (var i = 0; i <= 39; i += 8) {
                     displayFiveDay(futureForecastDiv, response2, i);
-                }
-
-
+                };
             });
         });
 
     });
 
+// ======================================================================================================================================
+
+    // FUNCTIONS
     function displayMainCard(apiData) {
+        var weatherContainer = $(`<div id="weatherContainer" class="col-md-8 ml-md-4 mx-auto"></div>`);
+        $('main').append(weatherContainer);
 
         var date = new Date(apiData.dt * 1000).toLocaleDateString();
         var currentCity = `<div id="currentCity" class="border my-3 py-3">` +
-            `<h2 class="ml-4 p-1 pb-1">${apiData.name} (${date}) <img src="http://openweathermap.org/img/wn/${apiData.weather[0].icon}@2x.png"></h2>` +
+            `<h1 id="cityName" ml-3 p-1>${apiData.name}<h1>` +
+            `<h2 class="ml-4 p-1 pb-1">(${date}) <img src="http://openweathermap.org/img/wn/${apiData.weather[0].icon}@2x.png"></h2>` +
             `<p class="ml-4"><strong>Temperature:</strong> ${apiData.main.temp.toFixed(1)} &deg;F</p>` +
-            `<p class="ml-4 p-1"><strong>Humidity:</strong>${apiData.main.humidity} %</p>` +
+            `<p class="ml-4 p-1"><strong>Humidity:</strong> ${apiData.main.humidity} %</p>` +
             `<p class="ml-4 p-1"><strong>Wind Speed:</strong> ${apiData.wind.speed} MPH</p>` +
             `<p class="ml-4 p-1"><strong>UV index:</strong>9.45</p>` +
             ` </div>`;
@@ -47,7 +57,7 @@ $(document).ready(function () {
         $('#weatherContainer').append(currentCity);
         $('#weatherContainer').append($('<h5 id="forecasth5">5-Day Forecast</h5>'));
 
-    }
+    };
 
     function displayFiveDay(futureForecastDiv, apiData, currentIndex) {
 
@@ -59,13 +69,19 @@ $(document).ready(function () {
             `</div>`);
         $('#weatherContainer').append(futureForecastDiv);
 
-        $('#fiveDays0').text(new Date(apiData.list[0].dt * 1000).toLocaleDateString());
-        $('#fiveDays8').text(new Date(apiData.list[8].dt * 1000).toLocaleDateString());
-        $('#fiveDays16').text(new Date(apiData.list[16].dt * 1000).toLocaleDateString());
-        $('#fiveDays24').text(new Date(apiData.list[24].dt * 1000).toLocaleDateString());
-        $('#fiveDays32').text(new Date(apiData.list[32].dt * 1000).toLocaleDateString());
+        $('#fiveDays0').text(new Date(apiData.list[7].dt * 1000).toLocaleDateString());
+        $('#fiveDays8').text(new Date(apiData.list[15].dt * 1000).toLocaleDateString());
+        $('#fiveDays16').text(new Date(apiData.list[23].dt * 1000).toLocaleDateString());
+        $('#fiveDays24').text(new Date(apiData.list[30].dt * 1000).toLocaleDateString());
+        $('#fiveDays32').text(new Date(apiData.list[39].dt * 1000).toLocaleDateString());
     };
 
-
+    function displayHistory() {
+        $('.search-history').empty();
+        for (var i = 0; i < cityHistoryArray.length; i++) {
+            var historyDiv = $(`<div class="border p-3" data-name="${cityHistoryArray[i]}">${cityHistoryArray[i]}</div>`);
+            $('.search-history').append(historyDiv);
+        }
+    }
 });
 
